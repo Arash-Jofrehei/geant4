4c1ee5186f395067d5e847c47b7b178c176a4f4b
Arash <arashjofrehei@yahoo.com>
2017-09-29 19:21:45 +0200

    latest 3x5 matrix from Arash

--- EEShashlikSimulation/Arash/matrix_Arash/cmake_command.sh
+++ EEShashlikSimulation/Arash/matrix_Arash/cmake_command.sh
@@ -11,4 +11,4 @@ mkdir ntuples
 #cmake -DGeant4_DIR=/Users/pandolf/local/geant4.10.00.p01-install/lib/Geant4-10.0.1/ ..
 cmake -DGeant4_DIR=/afs/cern.ch/sw/lcg/external/geant4/10.1.p01/x86_64-slc6-gcc49-opt/lib64/Geant4-10.1.1/ ..
 make -j 8
-./runEEShashlik -m ../../run2.mac
+#./runEEShashlik -m ../../run2.mac
